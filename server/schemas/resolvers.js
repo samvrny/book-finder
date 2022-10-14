d74cb3ff8e41b604d50e1bad10a3e8539d5825c2
samvrny <samvrny@gmail.com>
2022-10-14 18:10:19 -0500

    wrote out the skeleton typeDefs and resolvers. Lot of errors and bugs definitely going to need a lot of work

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,19 +1,45 @@
-const { User, Thought, Reaction } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
-const { signtToken } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
+const { User, Book } = require('../models');
 
 //create and define the resolvers 
 const resolvers = {
     //queries (the 'R' in the CRUD operations)
     Query: {
-        helloWorld: () => {
-            return 'Hello World';
+        me: async(parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('savedBooks');
+                
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in')
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('savedBooks');
         }
     },
-    //mutations ( the 'C, U, and D of the CRUD operations)
-    // Mutation: {
+    //mutations (the C,U,D of the CRUD operations)
+    Mutation: {
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
-    // }
+            return { token, user };
+        },
+        login: async(parent, { email, password}) => {
+            const user = await User.findOne({ email });
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            const token = signToken(user)
+            return { token, user };
+        },
+        //saveBook: async
+    }
 };
 
 //export the resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,40 @@ const { gql } = require('apollo-server-express');
 
 //create the typeDefs
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: Int
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
     type Query {
-        helloWorld: String
+        me: User
+        users: [User]
+        user(username: String!): User
+        savedBooks(username: String!): [Book]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser:(username: String!, email: String, password: String!): Auth
+        saveBook(authors: [String!], description: String!, title: String!, bookId: Int!, image: String!, link: String!): User
+        removeBook(bookId: Int!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
 `;
 
