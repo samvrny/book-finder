a5b1eab3f5723834ed19589eab778b2a176eadcb
samvrny <samvrny@gmail.com>
2022-10-15 16:48:11 -0500

    got the signup form working correctly

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 
 //import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -36,12 +37,12 @@ const SignupForm = () => {
         variables: { ...userFormData }
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       Auth.login(data.addUser.token);
-      console.log(user);
+      //console.log(user);
       // const { token, user } = await response.json();
       // console.log(user);
       // Auth.login(token);
